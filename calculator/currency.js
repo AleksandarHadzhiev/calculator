ed6f844bcfac493254463179c2c8cb695e7c676c
AleksandarHadzhiev <alekshadzhiev12@gmail.com>
2024-11-28 10:46:23 +0100

    feat: completed integration between frontend and backend

--- calculator/currency.js
+++ calculator/currency.js
@@ -14,26 +14,42 @@ function ToggleSidebar() {
     }
 }
 
+function updateConversionRate() {
+    const baseRate = document.getElementById('base-rate').value;
+    const conversionRate = document.getElementById('conversion-rate').value;
+    const rate = baseRate + "-" + conversionRate
+    console.log(rate)
+    currencyConverter.setNewConversionRate(rate)
+    convertRates()
+}
+
 function Back() {
-    var ev = document.getElementById('output');
-    ev.value = ev.value.slice(0, -1);
+    currentOutput = currentOutput.slice(0, -1);
+    convertRates()
+
 }
 function clearOutput() {
     currentOutput = '0';
-    document.getElementById('output').innerText = currentOutput;
+    document.getElementById('base-value').innerText = currentOutput;
+    convertRates()
+
 }
 
 function appendToOutput(value) {
     currentOutput += value
+    convertRates()
+}
+
+function convertRates() {
     currencyConverter.setNewBaseBalue(currentOutput)
     document.getElementById('base-value').innerText = currentOutput;
     const convertedValue = currencyConverter.getConvertedValue()
     console.log(convertedValue)
     document.getElementById('converted-value').innerText = convertedValue;
-
 }
 // Add the functions to the global `window` object
 window.ToggleSidebar = ToggleSidebar;
 window.Back = Back;
 window.clearOutput = clearOutput;
 window.appendToOutput = appendToOutput;
+window.updateConversionRate = updateConversionRate;
