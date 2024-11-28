4eba2b5d5c0ab7014551e3534bc28ce2118119b7
AleksandarHadzhiev <alekshadzhiev12@gmail.com>
2024-11-28 10:45:58 +0100

    feat: completed currency conversion class

--- calculator/classes/CurrencyConverter.js
+++ calculator/classes/CurrencyConverter.js
@@ -5,12 +5,20 @@ export class CurrencyConverter extends Converter {
         super(baseBalue, conversionRate)
         this.allowedConversionRates['euro-usd'] = this.convertEUROToUSD.bind(this)
         this.allowedConversionRates['usd-euro'] = this.convertUSDToEURO.bind(this)
-        //     this.allowedConversionRates['euro-leva'] = this.convertCurrency.bind(this)
-        //     this.allowedConversionRates['leva-euro'] = this.convertCurrency.bind(this)
-        //     this.allowedConversionRates['yen-usd'] = this.convertCurrency.bind(this)
-        //     this.allowedConversionRates['usd-yen'] = this.convertCurrency.bind(this)
-        //     this.allowedConversionRates['euro-yen'] = this.convertCurrency.bind(this)
-        //     this.allowedConversionRates['yen-euro'] = this.convertCurrency.bind(this)
+        this.allowedConversionRates['euro-leva'] = this.convertEUROToBGN.bind(this)
+        this.allowedConversionRates['leva-euro'] = this.convertBGNToEURO.bind(this)
+        this.allowedConversionRates['yen-usd'] = this.convertYENToUSD.bind(this)
+        this.allowedConversionRates['usd-yen'] = this.convertUSDToYEN.bind(this)
+        this.allowedConversionRates['usd-leva'] = this.convertUSDToBGN.bind(this)
+        this.allowedConversionRates['leva-usd'] = this.convertBGNToUSD.bind(this)
+        this.allowedConversionRates['euro-yen'] = this.convertEUROToYEN.bind(this)
+        this.allowedConversionRates['yen-euro'] = this.convertYENToEURO.bind(this)
+        this.allowedConversionRates['leva-yen'] = this.convertBGNToYEN.bind(this)
+        this.allowedConversionRates['yen-leva'] = this.convertYENToBGN.bind(this)
+        this.allowedConversionRates['usd-usd'] = this.noConvertionRequired.bind(this)
+        this.allowedConversionRates['euro-euro'] = this.noConvertionRequired.bind(this)
+        this.allowedConversionRates['leva-leva'] = this.noConvertionRequired.bind(this)
+        this.allowedConversionRates['yen-yen'] = this.noConvertionRequired.bind(this)
     }
 
     convertEUROToUSD() {
@@ -21,5 +29,60 @@ export class CurrencyConverter extends Converter {
     convertUSDToEURO() {
         this.convertedValue = this.baseBalue * 0.95;
         console.log(this.convertedValue)
+    }
+
+    convertEUROToYEN() {
+        this.convertedValue = this.baseBalue * 160.01;
+        console.log(this.convertedValue)
+    }
+
+    convertYENToEURO() {
+        this.convertedValue = this.baseBalue * 0.0062;
+        console.log(this.convertedValue)
+    }
+
+    convertBGNToYEN() {
+        this.convertedValue = this.baseBalue * 81.84;
+        console.log(this.convertedValue)
+    }
+
+    convertYENToBGN() {
+        this.convertedValue = this.baseBalue * 0.012;
+        console.log(this.convertedValue)
+    }
+
+    convertEUROToBGN() {
+        this.convertedValue = this.baseBalue * 1.96;
+        console.log(this.convertedValue)
+    }
+
+    convertBGNToEURO() {
+        this.convertedValue = this.baseBalue * 0.51;
+        console.log(this.convertedValue)
+    }
+
+    convertUSDToBGN() {
+        this.convertedValue = this.baseBalue * 1.86;
+        console.log(this.convertedValue)
+    }
+
+    convertBGNToUSD() {
+        this.convertedValue = this.baseBalue * 0.54;
+        console.log(this.convertedValue)
+    }
+
+
+    convertUSDToYEN() {
+        this.convertedValue = this.baseBalue * 151.85;
+        console.log(this.convertedValue)
+    }
+
+    convertYENToUSD() {
+        this.convertedValue = this.baseBalue * 0.0066;
+        console.log(this.convertedValue)
+    }
+
+    noConvertionRequired() {
+        this.convertedValue = this.baseBalue
     }
 }
