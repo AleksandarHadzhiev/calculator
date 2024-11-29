544cd42404981b4856923b2ae81fabb1e2f92f04
AleksandarHadzhiev <alekshadzhiev12@gmail.com>
2024-11-29 10:25:23 +0100

    ref: have a class to handle the action as it is the same for each converter

---
+++ calculator/classes/RateConverter.js
@@ -0,0 +1,31 @@
+export class RateConverter {
+    constructor(baseValue = 0, conversionRate = '') {
+        this.baseValue = baseValue
+        this.convertedValue = 0
+        this.conversionRate = conversionRate
+        this.conversionRates = {}
+    }
+
+    setConverionRates(newConversionRates) {
+        this.conversionRates = newConversionRates
+    }
+    getConvertedValue() {
+        return this.convertedValue
+    }
+
+    setNewBaseValue(newBaseValue) {
+        this.baseValue = newBaseValue
+        this.convertValue()
+    }
+
+    setNewConversionRate(newConversionRate,) {
+        this.conversionRate = newConversionRate
+        this.convertValue()
+    }
+
+    convertValue() {
+        const operation = `${this.baseValue} ${this.conversionRates[this.conversionRate]}`
+        this.convertedValue = eval(operation)
+    }
+
+}
---
+++ calculator/classes/RatesFileReader.js
@@ -0,0 +1,23 @@
+
+export async function loadRatesFromFile(lengths, jsonFileName) {
+    let conversionRates = {}
+    try {
+        const response = await fetch(jsonFileName);
+        const rates = await response.json();
+
+        // Structure the conversion rates with the key format "base-length"
+        lengths.forEach(baseLength => {
+            lengths.forEach(targetLength => {
+                const key = `${baseLength}-${targetLength}`;
+                if (rates[key] !== undefined) {
+                    conversionRates[key] = rates[key];
+                }
+            });
+        });
+        return conversionRates
+    } catch (error) {
+        throw new Error("Error loading conversion rates:", error.message);
+    }
+}
+
+
